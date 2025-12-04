5ffee239bddf736b0ebc2fc30357bbe96a5a6d13
Bonface Munyao <bonnieselaska18@gmail.com>
2025-12-04 10:57:51 +0300

    debug file 4

--- 4-concat.js
+++ 4-concat.js
@@ -1,1 +1,6 @@
-console.log((process.argv[2]  'undefined') + ' is ' + (process.argv[3]  'undefined'));
+// 4-concat.js
+console.log(
+  (process.argv[2] !== undefined ? process.argv[2] : 'undefined') +
+  ' is ' +
+  (process.argv[3] !== undefined ? process.argv[3] : 'undefined')
+);
